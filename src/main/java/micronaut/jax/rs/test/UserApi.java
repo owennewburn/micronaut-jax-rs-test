96e33b3ca18a744bf1c3a61d7d9cfbf07027a983
owen <owen.newburn@gmail.com>
2020-08-09 19:40:14 +0100

    stub jaxrs micronaut project

---
+++ src/main/java/micronaut/jax/rs/test/UserApi.java
@@ -0,0 +1,28 @@
+package com.owennewburn.generated.api;
+
+import java.util.UUID;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+
+import io.micronaut.http.annotation.PathVariable;
+import io.swagger.annotations.*;
+
+import java.io.InputStream;
+import java.util.Map;
+import java.util.List;
+import javax.validation.constraints.*;
+import javax.validation.Valid;
+
+@Path("/user")
+@Api(description = "the user API")
+public class UserApi {
+
+    @GET
+    @Path("/{user_id}")
+    @Produces({ "application/json" })
+    @ApiOperation(value = "Fetch a User by ID", notes = "", tags={ "user" })
+    String getUser(@PathParam("user_id") @ApiParam("UUID of the user to fetch") String userId){
+        throw new RuntimeException("Not Implemented");
+    }
+}
